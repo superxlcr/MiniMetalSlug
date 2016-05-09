5a81cb435e7a1ecbfd951f70693d354b3135b34e
superxlcr <405634593@qq.com>
2016-05-09 15:34:39 +0800

    finish graphics tools

--- app/src/main/java/com/github/superxlcr/minimetalslug/Utils/Graphics.java
+++ app/src/main/java/com/github/superxlcr/minimetalslug/Utils/Graphics.java
@@ -2,6 +2,7 @@ package com.github.superxlcr.minimetalslug.Utils;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
@@ -22,12 +23,24 @@ public final class Graphics {
         TRANS_MIRROR_ROT270
     }
 
-    public static final float INTERBAL_SCALE = 0.05f; // 缩放梯度
-    public static final int TIMES_SCALE = 20;
-    private static final float[] pts = new float[8];
-    private static final Path paht = new Path();
-    private static final RectF srcRect = new RectF();
+    public static final float INTERVAL_SCALE = 0.05f; // 缩放梯度
+    public static final int DEFAULT_TIMES_SCALE = 20; // 默认缩放大小,不缩放
 
+    /**
+     * 从源位图截取图片，变换后绘制
+     *
+     * @param canvas 画布
+     * @param src    源位图
+     * @param srcX   源位图x
+     * @param srcY   源位图y
+     * @param width  截取长度
+     * @param height 截取高度
+     * @param trans  变换类型
+     * @param drawX  绘制x
+     * @param drawY  绘制y
+     * @param degree 旋转角度
+     * @param scale  缩放大小
+     */
     public synchronized static void drawMatrixImage(Canvas canvas, Bitmap src,
             int srcX, int srcY, int width, int height, Trans trans, int drawX,
             int drawY, int degree, int scale) {
@@ -35,12 +48,14 @@ public final class Graphics {
             return;
         int srcWidth = src.getWidth();
         int srcHeight = src.getHeight();
+        // 截取长度高度超出上限
         if (srcX + width > srcWidth)
             width = srcWidth - srcX;
         if (srcY + height > srcHeight)
             height = srcHeight - srcY;
         if (width <= 0 || height <= 0)
             return;
+        // 判断变换状态
         int scaleX = scale;
         int scaleY = scale;
         int rotate = 0;
@@ -70,19 +85,78 @@ public final class Graphics {
                 rotate = 270;
                 break;
         }
+        // 无需变换情况
+        if (rotate == 0 && degree == 0 && scaleX == DEFAULT_TIMES_SCALE)
+            drawImage(canvas, src, srcX, srcY, width, height, drawX, drawY);
+        else {
+            Matrix matrix = new Matrix();
+            // 缩放
+            matrix.postScale(scaleX * INTERVAL_SCALE, scaleY * INTERVAL_SCALE);
+            // 旋转
+            matrix.postRotate(rotate);
+            matrix.postRotate(degree);
+            // 截取
+            RectF srcRectF = new RectF();
+            srcRectF.set(srcX, srcY, srcX + width, srcY + height);
+            matrix.mapRect(srcRectF);
+            // 移动
+            matrix.postTranslate(drawX - srcRectF.left, drawY - srcRectF.top);
+            // 左上，右上，右下，左下
+            float[] pts = new float[]{srcX, srcY, srcX + width, srcY,
+                    srcX + width, srcY + height, srcX, srcY + height};
+            matrix.mapPoints(pts);
+            // 截取画布
+            canvas.save();
+            Path path = new Path();
+            path.moveTo(pts[0], pts[1]);
+            path.lineTo(pts[2], pts[3]);
+            path.moveTo(pts[4], pts[5]);
+            path.moveTo(pts[6], pts[7]);
+            path.close();
+            canvas.clipPath(path);
+            // 使用matrix绘制位图
+            canvas.drawBitmap(src, matrix, null);
+            canvas.restore();
+        }
+    }
 
+    /**
+     * 图片缩放
+     *
+     * @param bitmap    原图
+     * @param newWidth  缩放后长度
+     * @param newHeight 缩放后宽度
+     * @return 缩放后图片
+     */
+    public static Bitmap scale(Bitmap bitmap, float newWidth, float newHeight) {
+        if (bitmap == null || bitmap.isRecycled())
+            return null;
+        float width = bitmap.getWidth();
+        float height = bitmap.getHeight();
+        if (width == 0 || height == 0 || newWidth == 0 || newHeight == 0)
+            return null;
+        // 缩放所需Matrix
+        Matrix matrix = new Matrix();
+        matrix.postScale(newWidth / width, newHeight / height);
+        try {
+            return Bitmap.createBitmap(bitmap, 0, 0, (int) width, (int) height,
+                                       matrix, true);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
      * 绘制位图
+     *
      * @param canvas 画布
-     * @param src 源位图
-     * @param srcX 位图截取x
-     * @param srcY 位图截取y
-     * @param width 截取长度
+     * @param src    源位图
+     * @param srcX   位图截取x
+     * @param srcY   位图截取y
+     * @param width  截取长度
      * @param height 截取高度
-     * @param drawX 绘制x
-     * @param drawY 绘制y
+     * @param drawX  绘制x
+     * @param drawY  绘制y
      */
     public synchronized static void drawImage(Canvas canvas, Bitmap src,
             int srcX, int srcY, int width, int height, int drawX, int drawY) {
@@ -108,6 +182,72 @@ public final class Graphics {
         dstRect.bottom = drawY + height;
         // 将源位图的srcRect区域取出绘制在dstRect
         canvas.drawBitmap(src, srcRect, dstRect, null);
+    }
+
+    /**
+     * 字符串对齐方式
+     */
+    public enum Anchor {
+        LEFT, RIGHT, HCENTER
+    }
+
+    /**
+     * 绘制字符串
+     *
+     * @param canvas   画布
+     * @param text     字符串
+     * @param textSize 字符串大小
+     * @param x        绘制x
+     * @param y        绘制y
+     * @param anchor   字符串对齐方式
+     * @param paint    画笔
+     */
+    public static void drawString(Canvas canvas, String text, float textSize,
+            float x, float y, Anchor anchor, Paint paint) {
+        // 设置字符串对齐方式
+        switch (anchor) {
+            case LEFT:
+                paint.setTextAlign(Paint.Align.LEFT);
+                break;
+            case RIGHT:
+                paint.setTextAlign(Paint.Align.RIGHT);
+                break;
+            case HCENTER:
+                paint.setTextAlign(Paint.Align.CENTER);
+                break;
+        }
+        // 设置字符串字体大小
+        paint.setTextSize(textSize);
+        // 绘制字符串
+        canvas.drawText(text, x, y, paint);
+    }
+
+    /**
+     * 绘制包边字符串
+     *
+     * @param canvas      画布
+     * @param borderColor 包边颜色
+     * @param textColor   字符串颜色
+     * @param text        字符串
+     * @param x           绘制x
+     * @param y           绘制y
+     * @param borderWidth 包边宽度
+     * @param paint       画笔
+     */
+    public static void drawBorderString(Canvas canvas, int borderColor,
+            int textColor, String text, float x, float y, int borderWidth,
+            Paint paint) {
+        // 开启抗锯齿
+        paint.setAntiAlias(true);
+        // 绘制字符串包边
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(borderWidth);
+        paint.setColor(borderColor);
+        canvas.drawText(text, x, y, paint);
+        // 绘制字符串
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(textColor);
+        canvas.drawText(text, x, y, paint);
     }
 
 }
